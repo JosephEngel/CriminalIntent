89be3affb0a756c398403abc68da57572108c5c3
joeyengel26 <joeyengel@zoho.com>
2017-11-21 01:51:01 -0500

    Add files via upload
    Chapter 7: UI Fragments and the Fragment Manager - Complete

---
+++ src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,33 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by joey on 11/20/17.
+ */
+
+public class Crime {
+
+    private UUID mID;
+
+    private String mTitle;
+
+
+    public Crime() {
+
+        // Generate unique identifier
+        mID = UUID.randomUUID();
+    }
+
+    public UUID getID() {
+        return mID;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
